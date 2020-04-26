287a6afb885edc133348e318f25affd77e2681b7
Annie Liew <anniebombanie@gmail.com>
2020-04-26 11:59:16 -0400

    clock works but hands needs refining

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function setDate() {
-  const now = new Date();
-  const s = now.getSeconds();
-  const sDegrees = ( s / 60 )* 360;
+const sHand = document.querySelector('.s');
+const mHand = document.querySelector('.m');
+const hHand = document.querySelector('.h');
+
+function setTime() {
+  const d = new Date();
+  const s = d.getSeconds();
+  const m = d.getMinutes();
+  const h = d.getHours();
+
+  const sDeg = (( s / 60 ) * 360 );
+  const mDeg = (( m / 60 ) * 360 );
+  const hDeg = (( h / 12 ) * 360 );
+
+  sHand.style.transform = `rotate( ${sDeg}deg )`;
+  mHand.style.transform = `rotate( ${mDeg}deg )`;
+  hHand.style.transform = `rotate( ${hDeg}deg )`;
+
+  console.log(sDeg)
 }
 
-setInterval( setDate, 1000 );
+setInterval( setTime, 1000 );
